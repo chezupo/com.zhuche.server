bd81f7d5dff9df6abf7227398207277d1aebaffa
wuchuheng <root@wuchuheng.com>
2022-03-19 02:24:15 +0800

    feat(minProgramApi): Created the api for fetching stores.

--- src/main/java/com/zhuche/server/api/v1/mini/program/Store.java
+++ src/main/java/com/zhuche/server/api/v1/mini/program/Store.java
@@ -3,21 +3,26 @@ package com.zhuche.server.api.v1.mini.program;
 import com.zhuche.server.dto.response.UnityResponse;
 import com.zhuche.server.services.MiniProgramStoreService;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.websocket.server.PathParam;
 
 @RestController("miniProgramStore")
 @RequestMapping("/api/v1/miniProgram/stores")
 @AllArgsConstructor
+@Validated
 public class Store {
     private final MiniProgramStoreService miniProgramStoreService;
 
     @GetMapping
     public UnityResponse getStores(
-        @PathParam("keyword") String keyword
+        @PathParam("keyword") @NotBlank String keyword
     ) {
         final var stores =  miniProgramStoreService.getStores(keyword);
         return UnityResponse.builder()
--- src/main/java/com/zhuche/server/services/MiniProgramStoreService.java
+++ src/main/java/com/zhuche/server/services/MiniProgramStoreService.java
@@ -3,18 +3,39 @@ package com.zhuche.server.services;
 import com.zhuche.server.model.Store;
 import com.zhuche.server.repositories.StoreRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class MiniProgramStoreService {
-        private final StoreRepository storeRepository;
+    private final StoreRepository storeRepository;
 
     public List<Store> getStores(String keyword) {
-        return null;
-//        storeRepository.getStores()
+        // dynamic query
+        Specification<Store> sf = (root, query, builder) -> {
+            final var newKeyword = "%" + keyword + "%";
+            List<Predicate> maps = new ArrayList<>();
+            maps.add(builder.like(root.get("address").as(String.class), newKeyword));
+            maps.add(builder.like(root.get("name").as(String.class), newKeyword));
+            maps.add(builder.like(root.get("city").get("name").as(String.class), newKeyword));
+            maps.add(builder.like(root.get("area").get("name").as(String.class), newKeyword));
+
+            Predicate[] pre = new Predicate[maps.size()];
+            Predicate or = builder.or(maps.toArray(pre));
+            query.where(or);
+            List<Order> orders = new ArrayList<>();
+            orders.add(builder.desc(root.get("id")));
+
+            return query.orderBy(orders).getRestriction();
+        };
+
+        return storeRepository.findAll(sf);
     }
 
 }

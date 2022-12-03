4f1c52c72754da3e0d2978b3c714c203bd1d6747
wuchuheng <root@wuchuheng.com>
2022-12-03 09:34:15 +0800

    refactor(admin): 重构违章功能

--- src/main/java/com/zhuche/server/services/ViolationService.java
+++ src/main/java/com/zhuche/server/services/ViolationService.java
@@ -1,5 +1,6 @@
 package com.zhuche.server.services;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
@@ -34,50 +35,75 @@ public class ViolationService {
    @Autowired private  PaginationUtil paginationUtil;
    @Autowired private  JWTUtil jwtUtil;
 
+    /**
+     * 添加违章
+     * @param id
+     * @param request
+     * @return
+     * @throws AlipayApiException
+     */
     @Transactional
-    public Violation createViolation(Long id, CreateViolationRequest request) throws AlipayApiException {
+    public Violation createViolation(Long id, CreateViolationRequest request) {
         final Order order = orderRepository.findById(id).get();
-        if (order.getUnfreezeAmount() < request.getAmount()) {
-            throw new MyRuntimeException(ExceptionCodeConfig.INTERIOR_ERROR_TYPE, String.format(
-                "请求解冻资金%s已经超剩下未解冻的额度%s",
-                request.getAmount(),
-                order.getUnfreezeAmount()
-            ));
-        }
-        AlipayTradePayRequest alipayRequest = new AlipayTradePayRequest();
-        alipayRequest.setNotifyUrl(alipayViolationNoticeUrl);
-        JSONObject bizContent = new JSONObject();
-        TradeUtil.generateOutTradeNo();
-        order.setUnfreezeAmount(order.getUnfreezeAmount() - request.getAmount());
+        final String images = JSON.toJSONString( request.getImages() );
         final Violation violation = Violation.builder()
             .title(request.getTitle())
             .description(request.getDescription())
             .amount(request.getAmount())
             .alipayOutTradeNo(TradeUtil.generateOutTradeNo())
             .freezeAmount(order.getUnfreezeAmount())
+            .images(images)
             .user(order.getUser())
             .createdAt(Timestamp.valueOf(LocalDateTime.now()).toInstant().toEpochMilli())
             .order(order)
             .build();
-        violation.setImages(request.getImages());
-        bizContent.put("out_trade_no", violation.getAlipayOutTradeNo());
-        bizContent.put("total_amount", violation.getAmount());
-        bizContent.put("subject", String.format(
-            "%s-违章扣款",
-            order.getTitle()
-        ));
-        bizContent.put("product_code", "PRE_AUTH_ONLINE");
-        bizContent.put("auth_no", order.getAuthNo());
-        bizContent.put("auth_confirm_mode", "NOT_COMPLETE");
-        alipayRequest.setBizContent(bizContent.toString());
-        AlipayTradePayResponse response = alipayClient.certificateExecute(alipayRequest);
-        if(response.isSuccess()){
-            orderRepository.save(order);
-            return violationRepository.save(violation);
-        } else {
-            throw new MyRuntimeException(ExceptionCodeConfig.INTERIOR_ERROR_TYPE, "调用失败");
-        }
+        violationRepository.save(violation);
+
+        return violation;
     }
+//    public Violation createViolation(Long id, CreateViolationRequest request) throws AlipayApiException {
+//        final Order order = orderRepository.findById(id).get();
+//        if (order.getUnfreezeAmount() < request.getAmount()) {
+//            throw new MyRuntimeException(ExceptionCodeConfig.INTERIOR_ERROR_TYPE, String.format(
+//                "请求解冻资金%s已经超剩下未解冻的额度%s",
+//                request.getAmount(),
+//                order.getUnfreezeAmount()
+//            ));
+//        }
+//        AlipayTradePayRequest alipayRequest = new AlipayTradePayRequest();
+//        alipayRequest.setNotifyUrl(alipayViolationNoticeUrl);
+//        JSONObject bizContent = new JSONObject();
+//        TradeUtil.generateOutTradeNo();
+//        order.setUnfreezeAmount(order.getUnfreezeAmount() - request.getAmount());
+//        final Violation violation = Violation.builder()
+//            .title(request.getTitle())
+//            .description(request.getDescription())
+//            .amount(request.getAmount())
+//            .alipayOutTradeNo(TradeUtil.generateOutTradeNo())
+//            .freezeAmount(order.getUnfreezeAmount())
+//            .user(order.getUser())
+//            .createdAt(Timestamp.valueOf(LocalDateTime.now()).toInstant().toEpochMilli())
+//            .order(order)
+//            .build();
+//        violation.setImages(request.getImages());
+//        bizContent.put("out_trade_no", violation.getAlipayOutTradeNo());
+//        bizContent.put("total_amount", violation.getAmount());
+//        bizContent.put("subject", String.format(
+//            "%s-违章扣款",
+//            order.getTitle()
+//        ));
+//        bizContent.put("product_code", "PRE_AUTH_ONLINE");
+//        bizContent.put("auth_no", order.getAuthNo());
+//        bizContent.put("auth_confirm_mode", "NOT_COMPLETE");
+//        alipayRequest.setBizContent(bizContent.toString());
+//        AlipayTradePayResponse response = alipayClient.certificateExecute(alipayRequest);
+//        if(response.isSuccess()){
+//            orderRepository.save(order);
+//            return violationRepository.save(violation);
+//        } else {
+//            throw new MyRuntimeException(ExceptionCodeConfig.INTERIOR_ERROR_TYPE, "调用失败");
+//        }
+//    }
 
     /**
      * 获取违章分页记录
